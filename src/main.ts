504035eff322ee736e7fdbc3865635a57f5fc928
frayfray248 <nicman248@hotmail.com>
2026-06-05 14:55:21 -0700

    initial vite app

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+Hooks.once("init", () => {
+
+    console.log("Vagabond | Initializing Vagabond VTT System");
+    
+})
---
+++ vite.config.js
@@ -0,0 +1,86 @@
+import { defineConfig } from "vite";
+import path from "path";
+import fs from "fs";
+
+const copyFiles = [
+    "system.json",
+]
+
+const copyDirectories = [
+    "styles",
+    "lang"
+]
+
+const copyFilesPlugin = {
+    name: "copy-system-json",
+    apply: "build",
+    writeBundle() {
+        copyFiles.forEach((file) => {
+            fs.copyFileSync(
+                path.resolve(__dirname, file),
+                path.resolve(__dirname, `dist/${file}`)
+            );
+        });
+
+        copyDirectories.forEach((dir) => {
+            fs.cpSync(
+                path.resolve(__dirname, `src/${dir}`),
+                path.resolve(__dirname, `dist/${dir}`),
+                { recursive: true, force: true }
+            );
+        });
+    }
+}
+
+export default defineConfig({
+    plugins: [copyFilesPlugin],
+    root: "src",
+    base: "/systems/vagabond-vtt-system/",
+    resolve: {
+        alias: {
+            "@client": path.resolve(__dirname, "foundry/client"),
+            "@common": path.resolve(__dirname, "foundry/common"),
+        }
+    },
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, "src/main.ts"),
+            name: "vagabond-vtt-system",
+            fileName: () => "main.js",
+            formats: ["es"]
+        },
+        rollupOptions: {
+            //input: './src/main.ts',
+            external: [/^foundry/],
+            output: {
+                assetFileNames: (assetInfo) => {
+                    console.log(assetInfo);
+                    if (assetInfo.names[0] === 'vagabond-vtt-system.css') return 'style.css';
+                    return assetInfo.names[0]
+                }
+            }
+        }
+    },
+    esbuild: {
+        minifyIdentifiers: false,
+        keepNames: true
+    },
+    server: {
+        port: 30001,
+        strictPort: true,
+        proxy: {
+            "^/systems/vagabond-vtt-system/dist": {
+                target: "http://localhost:30001",
+                rewrite: (path) => path.replace(/^\/systems\/vagabond-vtt-system\/dist/, "/systems/vagabond-vtt-system")
+            },
+            "^(?!/systems/vagabond-vtt-system(?!/lang/en\\.json$))": "http://localhost:30000/",
+            "/socket.io": {
+                target: "ws://localhost:30000",
+                ws: true,
+            },
+        }
+    }
+});
