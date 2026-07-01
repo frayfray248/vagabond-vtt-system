004a4548f62563c6ee3fc2c0e8af72775c8fcff9
frayfray248 <nicman248@hotmail.com>
2026-07-01 14:09:40 -0700

    Add Hero class extending SystemActor with derived data preparation

--- src/module/actor/document.ts
+++ src/module/actor/document.ts
@@ -1,5 +1,23 @@
-type SystemSubType = "base" | foundry.abstract.Document.ModuleSubType;
+import SystemActorDataModel, { SystemActorDataSchema, SystemActorSystemData } from "./model";
 
-export default class SystemActor<SubType extends SystemSubType = SystemSubType> extends Actor<SubType> {
+export default class SystemActor<
+DataModelType extends SystemActorDataModel<SystemActorDataSchema, SystemActorSystemData>,
+SystemActorType extends Actor.SubType = Actor.SubType,
+> extends Actor<SystemActorType> {
+
+
+    getSystem(): DataModelType {
+        return this.system as DataModelType;
+    }
+
+    toPlainObject() {
+        const system = this.getSystem();
+        return {
+            system: system.toPlainObject() as ReturnType<DataModelType["toPlainObject"]>,
+            name: this.name,
+        }
+    }
+
     
+
 }
