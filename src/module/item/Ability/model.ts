1d8f529b8dda281bf946f72476277afb2579c84a
frayfray248 <nicman248@hotmail.com>
2026-07-18 15:43:05 -0700

    feat: enhance AbilityDataModel with modifiers schema and data handling

--- src/module/item/Ability/model.ts
+++ src/module/item/Ability/model.ts
@@ -1,9 +1,49 @@
-import SystemItemDataModel from "../model";
+import SystemItemDataModel, { SystemItemDataSchema, SystemItemSystemData } from "../model";
 
-export default class AbilityDataModel extends SystemItemDataModel {
+const { NumberField, StringField, SchemaField, ArrayField } = foundry.data.fields;
+
+export type AbilityModifierMode = "add" | "subtract" | "multiply" | "divide";
+
+export interface AbilityModifierSchema extends foundry.data.fields.DataSchema {
+    target: InstanceType<typeof StringField<{}, {}, string>>;
+    value: InstanceType<typeof NumberField<{}, {}, number>>;
+    mode: InstanceType<typeof StringField<{}, {}, AbilityModifierMode>>;
+}
+
+export interface AbilityDataSchema extends SystemItemDataSchema {
+    modifiers: InstanceType<typeof ArrayField<InstanceType<typeof SchemaField<AbilityModifierSchema>>>>;
+}
+
+export type AbilitySystemData = SystemItemSystemData & {
+    modifiers: {
+        target: string;
+        value: number;
+        mode: AbilityModifierMode;
+    }[];
+}
+
+
+export default class AbilityDataModel extends SystemItemDataModel<AbilityDataSchema, AbilitySystemData> {
     static defineSchema() {
         return {
-            ...super.defineSchema()
+            ...super.defineSchema(),
+            modifiers: new ArrayField(
+                new SchemaField<AbilityModifierSchema>({
+                    target: new StringField<{}, {}, string>({
+                        required: true,
+                    }),
+                    value: new NumberField<{}, {}, number>({
+                        required: true,
+                        initial: 0
+                    }),
+                    mode: new StringField<{}, {}, AbilityModifierMode>({
+                        required: true,
+                        choices: ["add", "subtract", "multiply", "divide"],
+                        initial: "add"
+                    })
+                })
+            )
+
         }
     }
 }
--- src/module/item/model.ts
+++ src/module/item/model.ts
@@ -1,8 +1,26 @@
 import SystemBaseDataModel from "@/module/model";
 
-export default class SystemItemDataModel extends SystemBaseDataModel<{}, {}> {
+const { NumberField, StringField, SchemaField, ArrayField } = foundry.data.fields;
+
+export interface SystemItemDataSchema extends foundry.data.fields.DataSchema {
+    description: InstanceType<typeof StringField<{}, {}, string>>;
+}
+
+export type SystemItemSystemData = {
+    description: string;
+}
+
+export default class SystemItemDataModel
+<Schema extends SystemItemDataSchema,
+SystemDataType extends SystemItemSystemData
+>
+ extends SystemBaseDataModel<Schema, SystemDataType> {
     static defineSchema() {
         return {
+            description: new StringField<{}, {}, string>({
+                required: true,
+                initial: ""
+            })
         }
     }
 }
