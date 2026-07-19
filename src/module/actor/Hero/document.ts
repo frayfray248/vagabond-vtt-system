e069d90a3cbf6aa560ccafa9fc3a9c04b5e72ed0
frayfray248 <nicman248@hotmail.com>
2026-07-19 08:43:08 -0700

    refactor: remove unused Hero and Nonhero classes, clean up SystemActor imports

--- src/module/actor/Hero/document.ts
+++
@@ -1,9 +0,0 @@
-import SystemActor from "../document";
-import HeroDataModel, { HeroSystemData } from "./model";
-
-export default class Hero extends SystemActor<"hero"> {
-
-    prepareDerivedData(): void {
-        super.prepareDerivedData();
-    }
-}
--- src/module/actor/Nonhero/document.ts
+++
@@ -1,9 +0,0 @@
-import SystemActor from "../document";
-import NonheroDataModel from "./model";
-
-export default class Nonhero extends SystemActor<"nonhero"> {
-
-    prepareDerivedData(): void {
-        super.prepareDerivedData();
-    }
-}
